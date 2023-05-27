ef8689140d955a8ea9d5648438ad016148f37c60
Daniel Gomes <76263577+dcgo15@users.noreply.github.com>
2023-05-27 19:39:09 -0300

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,6 @@
-
 # O __init__ é o incializador do programa: main.py
+
+from flask import Flask
+
+
+app = Flask(__name__)
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
+'''
+
+	Criado por: dcgo15 
+
+
+'''
+
+
 # O código principal
 
-from flask import flask
-
+from flask import flask, render_template
+from app import app, models
 
 
 # Aqui é uma das rotas, aqui ficará a landing page
@@ -10,7 +18,105 @@ from flask import flask
 
 @app.route("/")
 def inicio():
-	return render_template("site.html") # Criar mais tarde
+	return render_template("site.html") 
+
+
+@app.route("/nao_encontrada")
+def error():
+	return render_template("error.html")
+
+
+# Area dos formulários de login e sign
+
+@app.route("/options")
+def opcao_conta():
+	return render_template("opcao.html")
+
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/nova_conta")
+def signup():
+	return render_template("signup.html")
+
+
+
+# Conta
+
+@app.route("/user/<id>")
+def ver_conta(id):
+	return render_template("ver_perfil.html")
+
+
+@app.route("/conta")
+def conta():
+	return render_template("conta.html")
+
+@app.route("/logout")
+def logout():
+	pass
+
+
+# Forum
+
+@app.route("/forum")
+def forum():
+	return render_template("forum.html")
+
+@app.route("/questao/<id>")
+def questao(id):
+	return render_template("ver_pergunta.html")
+
+@app.route("/questao/novo")
+def criar_questao():
+	return render_template("create_pergunta.html")
+
+
+# Cursos
+
+@app.route("/cursos")
+def curso():
+	return render_template("curso.html")
+
+@app.route("/cursos/<id>")
+def ver_curso(id):
+	return render_template("ver_curso.html")
+
+@app.route("/cursos/novo")
+def criar_curso():
+	return render_template("create_curso.html")
+
+
+# Jogos
+
+@app.route("/jogos")
+def jogos():
+	return render_template("jogos.html")
+
+@app.route("/jogos/<id>")
+def ver_jogosos(id):
+	return render_template("ver_jogo.html")
+
+@app.route("/jogos/novo")
+def create_jogos():
+	return render_template("create_jogo.html")
+
+
+# Comunidade
+
+@app.route("/grupos")
+def grupos():
+	return render_template("chat.html")
+
+@app.route("/grupos/<id>")
+def ver_grupos(id):
+	return render_template("ver_grupo.html")
+
+@app.route("/grupos/novo")
+def create_grupos():
+	return render_template("create_grupo.html")
 
 
 # Esse trecho roda o site, podemos usar na forma simplificada:
